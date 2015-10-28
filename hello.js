6adac5d5a081d1da94a81c4c97211889ebf0fdbd
Sri Varsha <CaptainVarsha@Sris-MacBook-Pro.local>
2015-10-28 13:00:11 -0500

    code pushed

--- hello.js
+++ hello.js
@@ -3,9 +3,17 @@ var pg = require('pg');
 var connectionString = process.env.DATABASE_URL || 'postgres://bwlfpbuceahrcc:d9ovRGxSKP2k5vE6B9sko_AncH@ec2-107-21-219-235.compute-1.amazonaws.com:5432/d89qomq57r5vei?ssl=true';
 var client = new pg.Client(connectionString);
 client.connect();
+fs = require('fs')
+fs.readFile('./CPH-T.csv', 'utf8', function (err,data) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log(data);
+});
 
-//var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
- var query = client.query("INSERT INTO items(text, complete) values($1, $1)", [data.text, data.complete]);
+
+var query = client.query('CREATE TABLE csvtable (id SERIAL PRIMARY KEY, county VARCHAR(40) not null,city VARCHAR(40) not null,year1 VARCHAR(40) not null,year2 VARCHAR(40) not null,year3 VARCHAR(40) not null,status VARCHAR(40) not null )');
+// var query = client.query("INSERT INTO items(text, complete) values($1, $2)", [data.text, data.complete]);
 
 query.on('row', function(row) {
     results.push(row);
