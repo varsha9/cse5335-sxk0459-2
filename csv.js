346c9277c93df6a2a34adf43c6e32dc2720780af
Sri Varsha <CaptainVarsha@Sris-MacBook-Pro.local>
2015-10-28 12:46:14 -0500

    code pushed

--- csv.js
+++ csv.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 
 
 var csvStream = csv.createStream();
-fs.createReadStream('/Users/CaptainVarsha/cse5335-sxk0459-2/CPH-T.csv').pipe(csvStream)
+fs.createReadStream('./CPH-T.csv').pipe(csvStream)
     .on('error',function(err){
         console.error(err);
     })
